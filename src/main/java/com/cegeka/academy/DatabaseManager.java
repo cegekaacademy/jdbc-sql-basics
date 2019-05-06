dd30eedb7853acc0320aea5031def443780896bc
Dita Alex <ditza_alex@yahoo.com>
2019-05-06 20:35:04 +0300

    initial commit

---
+++ src/main/java/com/cegeka/academy/DatabaseManager.java
@@ -0,0 +1,164 @@
+package com.cegeka.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/cegeka",
+                        "root", "admin123!@#");
+    }
+
+    public static void insertWithStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Insert with Statement");
+
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+
+        //int result = statement.executeUpdate("insert into team values(1001,'CegekaAcademy',20)");
+        //int result = statement.executeUpdate("insert into team values(null,'CegekaAcademy',20)");
+        int result = statement.executeUpdate("insert into team(name,points) values('CegekaAcademy',20)");
+        System.out.println("Result: " + result);
+
+        statement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void insertWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Insert with Prepared Statement");
+
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into team(name,points) values(?,?)");
+        preparedStatement.setString(1, "Cegeka Academy 2");
+        preparedStatement.setInt(2, 30);
+
+        int result = preparedStatement.executeUpdate();
+        System.out.println("Result: " + result);
+
+        preparedStatement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void selectWithStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Select with Statement");
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+
+        System.out.println("Select all");
+        ResultSet resultSet = statement.executeQuery("select * from team");
+        showResultSetValues(resultSet);
+
+        System.out.println("\nSelect By name");
+        resultSet = statement.executeQuery("select * from team where name='FCSB'");
+        showResultSetValues(resultSet);
+
+        statement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+
+    }
+
+    public static void selectWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Select with Prepared Statement");
+        Connection connection = getConnection();
+
+        System.out.println("Select all");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from team");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        showResultSetValues(resultSet);
+
+        System.out.println("\nSelect by name");
+        preparedStatement = connection.prepareStatement("select * from team where name=?");
+        preparedStatement.setString(1, "FCSB");
+        resultSet = preparedStatement.executeQuery();
+        showResultSetValues(resultSet);
+
+        preparedStatement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void deleteWithStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Delete with Statement");
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+
+        int result = statement.executeUpdate("delete from team where id=1006");
+        System.out.println("Result: " + result + "\n");
+        ResultSet resultSet = statement.executeQuery("select * from team");
+        showResultSetValues(resultSet);
+
+        statement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void deleteWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Delete with Prepared Statement");
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from team where id=?");
+        preparedStatement.setLong(1, 1007);
+
+        int result = preparedStatement.executeUpdate();
+        System.out.println("Result: " + result + "\n");
+
+        preparedStatement = connection.prepareStatement("select * from team");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        showResultSetValues(resultSet);
+
+        preparedStatement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void updateWithStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Update with statement");
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+
+        int result = statement.executeUpdate("update team set name='Cegeka Academy 3' where name='CegekaAcademy'");
+        System.out.println("Result: " + result + "\n");
+        ResultSet resultSet = statement.executeQuery("select * from team");
+        showResultSetValues(resultSet);
+
+        statement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void updateWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        System.out.println("Update with prepared statement");
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update team set name=? where name=?");
+        preparedStatement.setString(1, "Cegeka Academy 4");
+        preparedStatement.setString(2, "Cegeka Academy 2");
+
+        int result = preparedStatement.executeUpdate();
+        preparedStatement = connection.prepareStatement("select * from team");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.println("Result: " + result + "\n");
+        showResultSetValues(resultSet);
+
+        preparedStatement.close();
+        connection.close();
+        System.out.println("-----------------------------------------");
+    }
+
+    private static void showResultSetValues(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            Long id = resultSet.getLong("id");
+            String name = resultSet.getString("name");
+            Integer points = resultSet.getInt("points");
+
+            System.out.println("Team: " + id + ", " + name + ", " + points);
+        }
+    }
+}
