4d7af8acc7a8bba9762a061e7c829a7579becde4
Cosmin Alex <silvian1995@yahoo.com>
2019-05-07 20:30:31 +0300

    Add CRUD for player

--- src/main/java/com/cegeka/academy/Main.java
+++ src/main/java/com/cegeka/academy/Main.java
@@ -5,24 +5,45 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //Insert with statement
-        DatabaseManager.insertWithStatement();
-        //Insert with prepared statement
-        DatabaseManager.insertWithPreparedStatement();
-        //Select with statement
-        DatabaseManager.selectWithStatement();
-        //Select with prepared statement
-        DatabaseManager.selectWithPreparedStatement();
-        //Delete with statement
-        DatabaseManager.deleteWithStatement();
-        //Delete with prepared statement
-        DatabaseManager.deleteWithPreparedStatement();
-        //Update with statement
-        DatabaseManager.updateWithStatement();
-        //Update with prepared statement
-        DatabaseManager.updateWithPreparedStatement();
+//        //Insert with statement
+//        DatabaseManager.insertWithStatement();
+//        //Insert with prepared statement
+//        DatabaseManager.insertWithPreparedStatement();
+//        //Select with statement
+//        DatabaseManager.selectWithStatement();
+//        //Select with prepared statement
+//        DatabaseManager.selectWithPreparedStatement();
+//        //Delete with statement
+//        DatabaseManager.deleteWithStatement();
+//        //Delete with prepared statement
+//        DatabaseManager.deleteWithPreparedStatement();
+//        //Update with statement
+//        DatabaseManager.updateWithStatement();
+//        //Update with prepared statement
+//        DatabaseManager.updateWithPreparedStatement();
+//
+//
 
-        //TODO CRUD for Player table
+
+        PlayerRepository.insertWithStatement();
+        //Insert with prepared statement
+        PlayerRepository.insertWithPreparedStatement();
+        //Select with statement
+        PlayerRepository.selectWithStatement();
+        //Select with prepared statement
+        PlayerRepository.selectWithPreparedStatement();
+        //Delete with statement
+        PlayerRepository.deleteWithStatement();
+        //Delete with prepared statement
+        PlayerRepository.deleteWithPreparedStatement();
+        //Update with statement
+        PlayerRepository.updateWithStatement();
+        //Update with prepared statement
+        PlayerRepository.updateWithPreparedStatement();
+
         //TODO display all players for a specified team
+
+
+        //TODO display number of players for each team
     }
 }
--- src/main/java/com/cegeka/academy/PlayerRepository.java
+++ src/main/java/com/cegeka/academy/PlayerRepository.java
@@ -2,23 +2,16 @@ package com.cegeka.academy;
 
 import java.sql.*;
 
-public class DatabaseManager {
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/cegeka_db?useSSL=false",
-                        "root", "Python2.7");
-    }
-
+public class PlayerRepository {
     public static void insertWithStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Insert with Statement");
 
-        Connection connection = getConnection();
+        Connection connection = DatabaseManager.getConnection();
         Statement statement = connection.createStatement();
 
         //int result = statement.executeUpdate("insert into team values(1001,'CegekaAcademy',20)");
         //int result = statement.executeUpdate("insert into team values(null,'CegekaAcademy',20)");
-        int result = statement.executeUpdate("insert into team(name,points) values('CegekaAcademy',20)");
+        int result = statement.executeUpdate("insert into player(name, number, id_team, position) values('CegekaAcademy',20, 1, 'Attack')");
         System.out.println("Result: " + result);
 
         statement.close();
@@ -29,10 +22,12 @@ public class DatabaseManager {
     public static void insertWithPreparedStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Insert with Prepared Statement");
 
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into team(name,points) values(?,?)");
+        Connection connection = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into player(name, number, id_team, position) values(?,?, ?, ?)");
         preparedStatement.setString(1, "Cegeka Academy 2");
         preparedStatement.setInt(2, 30);
+        preparedStatement.setInt(3, 1);
+        preparedStatement.setString(4, "Attack");
 
         int result = preparedStatement.executeUpdate();
         System.out.println("Result: " + result);
@@ -44,16 +39,16 @@ public class DatabaseManager {
 
     public static void selectWithStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Select with Statement");
-        Connection connection = getConnection();
+        Connection connection = DatabaseManager.getConnection();
         Statement statement = connection.createStatement();
 
         System.out.println("Select all");
-        ResultSet resultSet = statement.executeQuery("select * from team");
-        showResultSetValues(resultSet);
+        ResultSet resultSet = statement.executeQuery("select * from player");
+        showPlayerResultSetValues(resultSet);
 
         System.out.println("\nSelect By name");
-        resultSet = statement.executeQuery("select * from team where name='FCSB'");
-        showResultSetValues(resultSet);
+        resultSet = statement.executeQuery("select * from player where name LIKE 'Cegeka Academy%'");
+        showPlayerResultSetValues(resultSet);
 
         statement.close();
         connection.close();
@@ -63,18 +58,18 @@ public class DatabaseManager {
 
     public static void selectWithPreparedStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Select with Prepared Statement");
-        Connection connection = getConnection();
+        Connection connection = DatabaseManager.getConnection();
 
         System.out.println("Select all");
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from team");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from player");
         ResultSet resultSet = preparedStatement.executeQuery();
-        showResultSetValues(resultSet);
+        showPlayerResultSetValues(resultSet);
 
         System.out.println("\nSelect by name");
-        preparedStatement = connection.prepareStatement("select * from team where name=?");
-        preparedStatement.setString(1, "FCSB");
+        preparedStatement = connection.prepareStatement("select * from team where name LIKE ?");
+        preparedStatement.setString(1, "Cegeka Academy");
         resultSet = preparedStatement.executeQuery();
-        showResultSetValues(resultSet);
+        showPlayerResultSetValues(resultSet);
 
         preparedStatement.close();
         connection.close();
@@ -83,13 +78,13 @@ public class DatabaseManager {
 
     public static void deleteWithStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Delete with Statement");
-        Connection connection = getConnection();
+        Connection connection = DatabaseManager.getConnection();
         Statement statement = connection.createStatement();
 
-        int result = statement.executeUpdate("delete from team where id=1006");
+        int result = statement.executeUpdate("delete from player where id=1006");
         System.out.println("Result: " + result + "\n");
-        ResultSet resultSet = statement.executeQuery("select * from team");
-        showResultSetValues(resultSet);
+        ResultSet resultSet = statement.executeQuery("select * from player");
+        showPlayerResultSetValues(resultSet);
 
         statement.close();
         connection.close();
@@ -98,16 +93,16 @@ public class DatabaseManager {
 
     public static void deleteWithPreparedStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Delete with Prepared Statement");
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("delete from team where id=?");
+        Connection connection = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from player where id=?");
         preparedStatement.setLong(1, 1007);
 
         int result = preparedStatement.executeUpdate();
         System.out.println("Result: " + result + "\n");
 
-        preparedStatement = connection.prepareStatement("select * from team");
+        preparedStatement = connection.prepareStatement("select * from player");
         ResultSet resultSet = preparedStatement.executeQuery();
-        showResultSetValues(resultSet);
+        showPlayerResultSetValues(resultSet);
 
         preparedStatement.close();
         connection.close();
@@ -116,13 +111,13 @@ public class DatabaseManager {
 
     public static void updateWithStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Update with statement");
-        Connection connection = getConnection();
+        Connection connection = DatabaseManager.getConnection();
         Statement statement = connection.createStatement();
 
-        int result = statement.executeUpdate("update team set name='Cegeka Academy 3' where name='CegekaAcademy'");
+        int result = statement.executeUpdate("update player set name='Cegeka Academy 3' where name='CegekaAcademy'");
         System.out.println("Result: " + result + "\n");
-        ResultSet resultSet = statement.executeQuery("select * from team");
-        showResultSetValues(resultSet);
+        ResultSet resultSet = statement.executeQuery("select * from player");
+        showPlayerResultSetValues(resultSet);
 
         statement.close();
         connection.close();
@@ -131,29 +126,30 @@ public class DatabaseManager {
 
     public static void updateWithPreparedStatement() throws SQLException, ClassNotFoundException {
         System.out.println("Update with prepared statement");
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("update team set name=? where name=?");
+        Connection connection = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("update player set name=? where name=?");
         preparedStatement.setString(1, "Cegeka Academy 4");
         preparedStatement.setString(2, "Cegeka Academy 2");
 
         int result = preparedStatement.executeUpdate();
-        preparedStatement = connection.prepareStatement("select * from team");
+        preparedStatement = connection.prepareStatement("select * from player");
         ResultSet resultSet = preparedStatement.executeQuery();
         System.out.println("Result: " + result + "\n");
-        showResultSetValues(resultSet);
+        showPlayerResultSetValues(resultSet);
 
         preparedStatement.close();
         connection.close();
         System.out.println("-----------------------------------------");
     }
 
-    private static void showResultSetValues(ResultSet resultSet) throws SQLException {
+    private static void showPlayerResultSetValues(ResultSet resultSet) throws SQLException {
         while (resultSet.next()) {
             Long id = resultSet.getLong("id");
             String name = resultSet.getString("name");
-            Integer points = resultSet.getInt("points");
+            Integer number = resultSet.getInt("number");
+            String position = resultSet.getString("position");
 
-            System.out.println("Team: " + id + ", " + name + ", " + points);
+            System.out.println("Player: " + id + ", " + name + ", " + number + ", " + position);
         }
     }
 }
