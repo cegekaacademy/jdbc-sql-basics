3acdc9fe6ceb016b602860aca6537256c06a4f94
adrianantonescu <antonescu.adrian16@gmail.com>
2019-05-14 01:34:56 +0300

    tema

--- src/main/java/com/cegeka/academy/HomeworkDatabase.java
+++ src/main/java/com/cegeka/academy/HomeworkDatabase.java
@@ -1,8 +1,6 @@
 package com.cegeka.academy;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class HomeworkDatabase {
 
@@ -13,7 +11,55 @@ public class HomeworkDatabase {
                         "root", "admin123!@#");
     }
 
+    //ex1
 
+    public static void selectCitiesWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
 
+        PreparedStatement preparedStatement = connection.prepareStatement("select distinct city from station where city like ? " +
+                "or city like ? or city like ? " +
+                "or city like ? or city like ?");
+        preparedStatement.setString(1, "%a");
+        preparedStatement.setString(2, "%e");
+        preparedStatement.setString(3, "%i");
+        preparedStatement.setString(4, "%o");
+        preparedStatement.setString(5, "%u");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        showResultSetCities(resultSet);
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private static void showResultSetCities(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String city = resultSet.getString("city");
+
+            System.out.println("City: " + city);
+        }
+    }
+
+    //ex2
+
+    public static void selectEmployeesWithPreparedStatement() throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select name from employee where salary > ? and months < ? order by employee_id;");
+        preparedStatement.setInt(1, 2000);
+        preparedStatement.setInt(2, 10);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        showResultEmployees(resultSet);
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private static void showResultEmployees(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String name = resultSet.getString("name");
+
+            System.out.println("Employee: " + name);
+        }
+    }
 
 }
